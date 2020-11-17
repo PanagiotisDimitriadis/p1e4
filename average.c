165865aa0ff81dc72b1468da66c3c34a995be7c0
unknown <Panos.dim125@hotmail.com>
2020-11-17 23:05:15 +0200

    p1e4

---
+++ average.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+	int a,b;
+	printf("dwste duo akeraious");
+	scanf("%d %d,&a,&b");
+	float A =(a +b)/2;
+	printf("O M.O) twn duo akeraiwn einai:%f ", a);
+	return 0;
+}
---
+++ simpleinput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+	int a ;
+	
+	printf("dwste akeraio arithmo");
+	scanf("%d,&a");
+	printf("o akeraios arithmos einai:%d,a");
+	return 0;
+}
